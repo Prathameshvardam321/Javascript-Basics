390b613f97dc85ad86b352c66e3acb1859cacf9c
Prathameshvardam321 <prathameshvardam321@gmail.com>
2023-01-18 17:07:51 +0530

    Added Basic Datatypes

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+var a = BigInt("122333333333333333333333")
+var b = "Ok "+12+12
+var c =  12+12+" Ok "+23
+var ob1 = {
+    name : "Prathmesh",
+    state : "Maharshtra"
+}
+var array = ["kak","Prathmesh","ahaja"]
+
+console.log(a,typeof a);
+console.log(b,typeof b);
+console.log(c,typeof c);
+console.log(ob1,typeof ob1);
+console.log(array,typeof array);
